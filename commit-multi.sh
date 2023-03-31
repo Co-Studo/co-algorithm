5feb11b2186e5d5089c4aaef7f937fba0abf5d51
healtheloper <58503584+healtheloper@users.noreply.github.com>
2023-03-31 17:33:10 +0900

    env: add multi solution commit script

---
+++ commit-multi.sh
@@ -0,0 +1,23 @@
+ADD_FILES=$(git ls-files -o)
+
+re='^[0-9]+$'
+
+for FILE in ${ADD_FILES}
+do
+    GET_NUM=($(echo ${FILE}|tr "/" "\n"))
+    NUM_SPLIT=($(echo ${GET_NUM[1]}|tr "_" "\n"))
+
+    if ! [[ "${NUM_SPLIT[0]}" =~ $re ]]
+    then
+        echo "🥲 파일 형식에 맞지않는 파일이 포함되어 있습니다. > ${FILE}"
+    else
+        git add ${FILE}
+        echo "> ✅ add ${FILE}"
+        git commit -m "[$1] ${NUM_SPLIT[0]}"
+        echo "> ✅ commit complete"
+    fi
+done
+
+# git push origin main
+
+# echo "> ✅ push complete"
